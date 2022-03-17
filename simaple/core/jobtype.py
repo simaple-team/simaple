db4e514d0ed1da65d7166a865f781e6e97b4ecbc
jayden.ka <jayden.ka@kakaocorp.com>
2022-03-18 00:02:44 +0900

    feat: add job type

---
+++ simaple/core/jobtype.py
@@ -0,0 +1,56 @@
+import enum
+
+
+class JobType(enum.Enum):
+    hero = "hero"
+    darkknight = 'darkknight'
+    paladin = 'paladin'
+
+    bishop = 'bishop'
+    archmagefb = 'archmagefb'
+    archmagetc = 'archmagetc'
+
+    nightlord = 'nightlord'
+    shadower = 'shadower'
+    dualblade = 'dualblade'
+
+    bowmaster = 'bowmaster'
+    sniper = 'sniper'
+    pathfinder = 'pathfinder'
+
+    corsair = 'corsair' # Captain
+    buccaneer = 'buccaneer'  # viper
+    cannoneer = 'cannoneer' # Cannon shooter
+
+    soulmaster = 'soulmaster'
+    flamewizard = 'flamewizard'
+    windbreaker = 'windbreaker'
+    nightwalker = 'nightwalker'
+    striker = 'striker'
+
+    mihile = 'mihile'
+
+    demonavenger = 'demonavenger'
+    demonslayer = 'demonslayer'
+
+    evan = 'evan'
+    luminous = 'luminous'
+    mercedes = 'mercedes'
+    phantom = 'phantom'
+    eunwol = 'eunwol'
+    aran = 'aran'
+
+    kaiser = 'kaiser'
+    kain = 'kain'
+    cadena = 'cadena'
+    angelicbuster = 'angelicbuster'
+
+    adele = 'adele'
+    illium = 'illium'
+    ark = 'ark'
+
+    lara = 'lara'
+    hoyoung = 'hoyoung'
+
+    zero = 'zero'
+    kinesis = 'kinesis'
