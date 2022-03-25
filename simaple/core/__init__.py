68bdfff95530482fed8f30d4f0e283355e3c39c1
meson324 <meson3241@gmail.com>
2022-03-25 11:11:27 +0900

    feat: add job category

--- simaple/core/__init__.py
+++ simaple/core/__init__.py
@@ -1,2 +1,3 @@
 from simaple.core.base import ActionStat, AttackType, BaseStatType, Stat, StatProps
 from simaple.core.jobtype import JobType
+from simaple.core.job_category import JobCategory
---
+++ simaple/core/job_category.py
@@ -0,0 +1,8 @@
+import enum
+
+class JobCategory(enum.IntEnum):
+    warrior = 0
+    mage = 1
+    archer = 2
+    thief = 3
+    pirate = 4
