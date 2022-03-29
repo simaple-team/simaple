02f0ca1642882dc8a4fb6378b71876809c6c798c
meson324 <meson3241@gmail.com>
2022-03-30 03:16:56 +0900

    feat: add triple-potential optimizer
    Implemented logic is brute-force

--- simaple/optimizer/weapon_potential_optimizer.py
+++ simaple/optimizer/weapon_potential_optimizer.py
@@ -10,7 +10,8 @@ from simaple.gear.potential import Potential, PotentialTier
 
 _WEAPON_POTENTIALS = {
     PotentialTier.empty: [
-        Stat(),
+        Stat(attack_power=6),
+        Stat(magic_attack=6),
     ],
     PotentialTier.rare: [
         Stat(attack_power_multiplier=3),
@@ -42,12 +43,23 @@ class WeaponPotentialOptimizer(BaseModel):
     tiers: Tuple[PotentialTier, PotentialTier, PotentialTier]
     damage_logic: DamageLogic
     armor: int = 300
-    emblem: bool = False
+
+    def get_useful_candidates(self, tier):
+        candidates = [
+            stat
+            for stat in _WEAPON_POTENTIALS[tier]
+            if self.damage_logic.get_damage_factor(stat + self.default_stat)
+            - self.damage_logic.get_damage_factor(self.default_stat)
+            > 0
+        ]
+        return candidates
 
     def get_potential_candidates(
-        self, tiers: Tuple[PotentialTier, PotentialTier, PotentialTier]
+        self,
+        tiers: Tuple[PotentialTier, PotentialTier, PotentialTier],
+        emblem=False,
     ) -> Iterable[Potential]:
-        for stats in product(*[_WEAPON_POTENTIALS[tier] for tier in tiers]):
+        for stats in product(*[self.get_useful_candidates(tier) for tier in tiers]):
             boss_damage_multiplier_count = 0
             ignored_defence_count = 0
             for stat in stats:
@@ -56,16 +68,16 @@ class WeaponPotentialOptimizer(BaseModel):
                 if stat.ignored_defence > 0:
                     ignored_defence_count += 1
 
-            if self.emblem and boss_damage_multiplier_count > 0:
+            if emblem and boss_damage_multiplier_count > 0:
                 continue
-            
+
             if boss_damage_multiplier_count > 3 or ignored_defence_count > 3:
                 continue
 
             yield Potential(options=list(stats))
 
-    def get_cost(self, potential: Potential) -> float:
-        stat = self.default_stat + potential.get_stat()
+    def get_cost(self, potential_stat: Stat) -> float:
+        stat = self.default_stat + potential_stat
         return self.damage_logic.get_damage_factor(stat, armor=self.armor)
 
     def get_optimal_potential(self) -> Potential:
@@ -73,8 +85,38 @@ class WeaponPotentialOptimizer(BaseModel):
         optimal_cost = 0.0
 
         for potential in self.get_potential_candidates(self.tiers):
-            cost = self.get_cost(potential)
+            cost = self.get_cost(potential.get_stat())
             if cost > optimal_cost:
                 optimal_cost, optimal_potential = cost, potential
+
+        return optimal_potential
+
+    def get_full_optimal_potential(self) -> Tuple[Potential, Potential, Potential]:
+        optimal_potential = (Potential(), Potential(), Potential())
+        optimal_cost = 0.0
+
+        iter_count = 0
+
+        weapon_potential_candidates = list(self.get_potential_candidates(self.tiers))
+        sub_weapon_potential_candidates = list(self.get_potential_candidates(self.tiers))
+        emblem_potential_candidates = list(self.get_potential_candidates(self.tiers, emblem=True))
+        
+        for weapon_potential in weapon_potential_candidates:
+            layer1_cached_stat = weapon_potential.get_stat()
+            for sub_weapon_potential in sub_weapon_potential_candidates:
+                layer2_cached_stat = (
+                    layer1_cached_stat + sub_weapon_potential.get_stat()
+                )
+                for emblem_potential in emblem_potential_candidates:
+                    cost = self.get_cost(
+                        layer2_cached_stat + emblem_potential.get_stat()
+                    )
+                    iter_count += 1
+                    if cost > optimal_cost:
+                        optimal_cost, optimal_potential = cost, (
+                            weapon_potential,
+                            sub_weapon_potential,
+                            emblem_potential,
+                        )
 
         return optimal_potential
--- tests/optimizer/test_potential_optimizer.py
+++ tests/optimizer/test_potential_optimizer.py
@@ -37,4 +37,32 @@ def test_optimizer(tiers):
             damage_logic=INTBasedDamageLogic(attack_range_constant=1.0, mastery=0.95),
         )
         output = optimizer.get_optimal_potential()
-        logger.info(f"Optimization output {output}")
+
+
+@pytest.mark.parametrize(
+    "tiers",
+    [
+        (PotentialTier.unique, PotentialTier.unique, PotentialTier.unique),
+        (PotentialTier.unique, PotentialTier.epic, PotentialTier.epic),
+        (PotentialTier.legendary, PotentialTier.unique, PotentialTier.unique),
+        (PotentialTier.legendary, PotentialTier.unique, PotentialTier.unique),
+        (PotentialTier.legendary, PotentialTier.unique, PotentialTier.empty),
+    ],
+)
+def test_triple_optimizer(tiers):
+    with Timer():
+        optimizer = WeaponPotentialOptimizer(
+            default_stat=Stat(
+                INT=40000,
+                LUK=5000,
+                magic_attack=3000,
+                critical_rate=80,
+                critical_damage=100,
+                damage_multiplier=300,
+                ignored_defence=90,
+                INT_static=10000,
+            ),
+            tiers=tiers,
+            damage_logic=INTBasedDamageLogic(attack_range_constant=1.0, mastery=0.95),
+        )
+        output = optimizer.get_full_optimal_potential()
