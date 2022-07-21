6f79924d89697b3734ad9dc13bbdebcca0108068
oleneyl <39217532+oleneyl@users.noreply.github.com>
2022-07-21 22:17:36 +0900

    feat: add arcane symbol parser

--- simaple/fetch/application/base.py
+++ simaple/fetch/application/base.py
@@ -1,5 +1,12 @@
-from simaple.fetch.element.item import item_promise
-from simaple.fetch.element.item_list import maple_item_list_promise
+from loguru import logger
+
+from simaple.fetch.element import (
+    MapleItemListElement,
+    character_promise,
+    item_promise,
+    maple_item_list_promise,
+    pet_list_promise,
+)
 from simaple.fetch.token import TokenRepository
 
 
@@ -9,10 +16,15 @@ class Application:
 
 class KMSFetchApplication(Application):
     def run(self, name: str):
-        promise = maple_item_list_promise().then(
-            {idx: item_promise() for idx in range(35)}
-        )
         token_repository = TokenRepository()
         token = token_repository.get(name)
-        result = promise.resolve("", token)
-        print(result)
+
+        item = maple_item_list_promise().then(
+            {
+                idx: item_promise()
+                for idx in set(
+                    MapleItemListElement.expected_normal_names()
+                    + MapleItemListElement.expected_arcane_names()
+                )
+            }
+        )
--- simaple/fetch/element/item_list.py
+++ simaple/fetch/element/item_list.py
@@ -8,13 +8,53 @@ class MapleItemListElement(Element):
     def run(self, html_text: str):
         soup = BeautifulSoup(html_text, "html.parser")
 
+        result = {}
+        result.update(self.normal_items(soup))
+        result.update(self.arcane_symbols(soup))
+        return result
+
+    @classmethod
+    def expected_normal_names(cls):
+        # fmt: off
+        return [
+            "ring1", "", "cap", "", "emblem",
+            "ring2", "pendant2", "face_accessory", "", "badge",
+            "ring3", "pendant1", "eye_accessory", "earrings", "medal",
+            "ring4", "weapon", "coat", "shoulder_pad", "subweapon",
+            "pocket", "belt", "pants", "glove", "cape",
+            "", "", "shoes", "android", "machine_heart"
+        ]
+        # fmt: on
+
+    @classmethod
+    def expected_arcane_names(cls):
+        return [f"arcane_{idx}" for idx in range(6)]
+
+    def normal_items(self, soup):
         item_elements = soup.find(class_="item_pot").find_all("li")
         item_links = [item.find("a") for item in item_elements]
         item_urls = list(
             map(lambda element: element["href"] if element else "", item_links)
         )
 
-        return {idx: url for idx, url in enumerate(item_urls) if len(url) > 0}
+        return {
+            name: url
+            for name, url in zip(self.expected_normal_names(), item_urls)
+            if len(url) > 0
+        }
+
+    def arcane_symbols(self, soup):
+        item_elements = soup.find(class_="arcane_weapon_wrap").find_all("ac_pot01")
+        item_links = [item.find("a") for item in item_elements]
+        item_urls = list(
+            map(lambda element: element["href"] if element else "", item_links)
+        )
+
+        return {
+            name: url
+            for name, url in zip(self.expected_arcane_names(), item_urls)
+            if len(url) > 0
+        }
 
 
 def maple_item_list_promise():
