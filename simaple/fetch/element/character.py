34eecbb609d960dc4f082b20974ae1ab5a4c44b9
oleneyl <meson3241@gmail.com>
2022-07-23 14:17:44 +0900

    feat: santize number

--- simaple/fetch/element/character.py
+++ simaple/fetch/element/character.py
@@ -7,6 +7,28 @@ from simaple.fetch.element.base import Element, ElementWrapper
 from simaple.fetch.query import CookiedQuery
 
 
+class NumberString:
+    @classmethod
+    def english_number_to_int(cls, number_string: str) -> int:
+        return int(number_string.replace(",", ""))
+
+    @classmethod
+    def percentile_number_to_int(cls, number_string: str) -> int:
+        return int(number_string.replace("%", ""))
+
+    @classmethod
+    def number_string_to_int(cls, number_string: str) -> int:
+        return int(cls.sanitize(number_string))
+
+    @classmethod
+    def sanitize(cls, number_string: str) -> str:
+        return number_string.strip().replace(" ", "").replace(",", "").replace("%", "")
+
+    @classmethod
+    def is_number(cls, number_string: str) -> bool:
+        return str.isdigit(cls.sanitize(number_string))
+
+
 class CharacterElement(Element):
     def run(self, html_text: str) -> dict:
         soup = BeautifulSoup(html_text, "html.parser")
@@ -34,7 +56,14 @@ class CharacterElement(Element):
         result.update(self.get_hyperstat(hyperstat_element))
         result.update(self.get_trait(trait_element))
 
-        return result
+        sanitized_result = {}
+        for k, v in result.items():
+            if isinstance(v, str) and NumberString.is_number(v):
+                sanitized_result[k] = NumberString.number_string_to_int(v)
+            else:
+                sanitized_result[k] = v
+
+        return sanitized_result
 
     def get_class_info(self, class_element):
         name = class_element.find(class_="char_name").find("span").text.replace("님", "")
@@ -72,6 +101,10 @@ class CharacterElement(Element):
         for index_tuple, element in zip(index, stat_element):
             for index, td_element in zip(index_tuple, element.find_all("td")):
                 result[index] = td_element.text
+
+        min_damage_factor, max_damage_factor = result.pop("damage_factor").split("~")
+        result["min_damage_factor"] = min_damage_factor
+        result["max_damage_factor"] = max_damage_factor
 
         return result
 
