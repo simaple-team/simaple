f57478a6eafe14a5703e4d90b319db802993e473
oleneyl <meson3241@gmail.com>
2022-08-23 10:16:56 +0900

    refactor: refactor benchmark yaml
    refactor: remove duplicated code
    
    refactor: split patches
    
    refactor: seperate patches
    
    refactor: better readability
    
    refactor: set as implementation of spec
    
    refactor: move to benchmark again
    
    refactor: apply lint

---
+++ simaple/benchmark/spec/patch.py
@@ -0,0 +1,71 @@
+import json
+import os
+
+import pydantic
+
+from simaple.core import JobCategory
+from simaple.spec.patch import KeywordExtendPatch, Patch, StringPatch
+
+
+def kms_item_alias():
+    INTERPETER_RESOURCE_PATH = os.path.join(os.path.dirname(__file__), "resources")
+    path = os.path.join(INTERPETER_RESOURCE_PATH, "item_name_alias.json")
+
+    with open(path, encoding="utf-8") as f:
+        return json.load(f)
+
+
+class GearIdPatch(Patch):
+    job_category: JobCategory
+    item_name_alias: dict[str, list[str]] = pydantic.Field(
+        default_factory=kms_item_alias
+    )
+
+    def interpret_gear_id(self, name: str) -> str:
+        if name in self.item_name_alias:
+            return self.item_name_alias[name][self.job_category.value]
+
+        return name
+
+    def apply(self, raw):
+        if isinstance(raw, list):
+            return [self.apply(arg) for arg in raw]
+
+        if isinstance(raw, (int, float, str)):
+            return raw
+
+        interpreted = {}
+
+        for k, v in raw.items():
+            if isinstance(v, (dict, list)):
+                interpreted[k] = self.apply(v)
+            else:
+                if k == "gear_id":
+                    interpreted["gear_id"] = self.interpret_gear_id(v)
+                else:
+                    interpreted[k] = v
+
+        return interpreted
+
+
+def all_stat_patch():
+    return KeywordExtendPatch(
+        target_keyword="all_stat", extends=["STR", "DEX", "INT", "LUK"]
+    )
+
+
+def all_att_patch():
+    return KeywordExtendPatch(
+        target_keyword="all_att", extends=["attack_power", "magic_attack"]
+    )
+
+
+def stat_patch(stat_priority: tuple[str, str, str, str]):
+    return StringPatch(
+        as_is=["first_stat", "second_stat", "third_stat", "fourth_stat"],
+        to_be=stat_priority,
+    )
+
+
+def attack_patch(attack_priority: tuple[str, str, str, str]):
+    return StringPatch(as_is=["first_att", "second_att"], to_be=attack_priority)
--- tests/benchmark/test_interpreter.py
+++ tests/benchmark/test_interpreter.py
@@ -1,53 +1,40 @@
 import pytest
 
-from simaple.spec.interpreter import (
-    BenchmarkConfigurationInterpreter,
-    BenchmarkInterpreterOption,
-)
+from simaple.benchmark.spec.interpreter import builtin_blueprint
 from simaple.core import BaseStatType, JobCategory
 from simaple.gear.gear_repository import GearRepository
 
 
 @pytest.mark.parametrize(
-    "benchmark_file",
+    "benchmark_name",
     [
-        "./simaple/benchmark/builtin/Epic.yaml",
-        "./simaple/benchmark/builtin/EpicUnique.yaml",
-        "./simaple/benchmark/builtin/Unique.yaml",
-        "./simaple/benchmark/builtin/Legendary18.yaml",
-        "./simaple/benchmark/builtin/LegendaryHalf.yaml",
-        "./simaple/benchmark/builtin/Legendary.yaml",
+        "Epic",
+        "EpicUnique",
+        "Unique",
+        "Legendary18",
+        "LegendaryHalf",
+        "Legendary",
     ],
 )
-def test_interpreter_get_gearset(benchmark_file):
-    interpreter_option = BenchmarkInterpreterOption(
-        stat_priority=["STR", "DEX", "INT", "LUK"],
-        attack_priority=["attack_power", "magic_attack"],
+def test_interpreter_get_gearset(benchmark_name):
+    user_gearset_blueprint = builtin_blueprint(
+        benchmark_name,
         job_category=JobCategory.warrior,
     )
 
-    interpreter = BenchmarkConfigurationInterpreter()
-
-    user_gearset_blueprint = interpreter.interpret_user_gearset_from_file(
-        benchmark_file, interpreter_option
-    )
     gear_repository = GearRepository()
     gearset = user_gearset_blueprint.build(gear_repository)
 
 
 def test_gearset_ok():
-    interpreter_option = BenchmarkInterpreterOption(
-        stat_priority=["STR", "DEX", "INT", "LUK"],
-        attack_priority=["attack_power", "magic_attack"],
+    user_gearset_blueprint = builtin_blueprint(
+        "Legendary",
         job_category=JobCategory.warrior,
     )
 
-    interpreter = BenchmarkConfigurationInterpreter()
-
-    user_gearset_blueprint = interpreter.interpret_user_gearset_from_file(
-        "./simaple/benchmark/builtin/Legendary.yaml", interpreter_option
-    )
     gear_repository = GearRepository()
     gearset = user_gearset_blueprint.build(gear_repository)
 
-    print(gearset.get_total_stat().get_base_stat_coefficient(BaseStatType.STR))
+    assert (
+        gearset.get_total_stat().get_base_stat_coefficient(BaseStatType.STR) == 42760.2
+    )
