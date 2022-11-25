64a4925e9d5db74650403242c6576629e34c61f0
oleneyl <meson3241@gmail.com>
2022-11-25 19:55:04 +0900

    fix: scale bug

--- simaple/simulate/report/dpm.py
+++ simaple/simulate/report/dpm.py
@@ -7,14 +7,13 @@ import pydantic
 
 class DPMCalculator(pydantic.BaseModel):
     character_spec: Stat
-    v_skill_enhancements: dict[str, Stat]
     damage_logic: DamageLogic
     armor: int = 300
 
     def get_damage(self, log: DamageLog):
         buffed_stat = self.character_spec + log.buff
         damage_factor = self.damage_logic.get_damage_factor(buffed_stat, self.armor)
-        return log.damage * log.hit * damage_factor
+        return log.damage * log.hit * damage_factor * 0.01
 
     def calculate_damage(self, damage_report: Report):
         total_damage = 0
@@ -24,4 +23,4 @@ class DPMCalculator(pydantic.BaseModel):
         return total_damage
 
     def calculate_dpm(self, damage_report: Report):
-        return self.calculate_damage(damage_report) / damage_report.total_time
+        return self.calculate_damage(damage_report) / damage_report.total_time() * 60_000
