8801c745bb2fe9d9d14f211be64d9621937e4e95
oleneyl <meson3241@gmail.com>
2022-11-27 02:18:10 +0900

    feat: support level, force adventage

--- simaple/simulate/report/dpm.py
+++ simaple/simulate/report/dpm.py
@@ -5,15 +5,50 @@ from simaple.core.damage import DamageLogic
 from simaple.simulate.report.base import DamageLog, Report
 
 
+class LevelAdvantage:
+    def __init__(self):
+        # fmt: off
+        self._advantage_table = [
+            1.2, 1.18, 1.16, 1.14, 1.12, 1.1,
+            1.0584, 1.0070, 0.9672, 0.9180, 0.88,
+            .85, .83, .80, .78, .75,
+            .73, .70, .68, .65, .63,
+            .60, .58, .55, .53, .50,
+            .48, .45, .43, .40, .38,
+            .35, .33, .30, .28, .25,
+            .23, .20, .18, .15, .13,
+            .10, .08, .05, .02, .00
+        ]
+        # fmt: on
+        self._bias = 5
+
+    def get_advantage(self, mob_level: int, character_level: int) -> float:
+        advantage_index = mob_level - character_level + self._bias
+        if advantage_index < 0:
+            return self._advantage_table[0]
+        if advantage_index > len(self._advantage_table):
+            return 0.0
+
+        return self._advantage_table[advantage_index]
+
+
 class DPMCalculator(pydantic.BaseModel):
     character_spec: Stat
     damage_logic: DamageLogic
     armor: int = 300
+    level_advantage: float = 1.0
+    force_advantage: float = 1.0
 
     def get_damage(self, log: DamageLog):
         buffed_stat = self.character_spec + log.buff
         damage_factor = self.damage_logic.get_damage_factor(buffed_stat, self.armor)
-        return (log.damage * 0.01) * log.hit * damage_factor
+        return (
+            (log.damage * 0.01)
+            * log.hit
+            * damage_factor
+            * self.level_advantage
+            * self.force_advantage
+        )
 
     def calculate_damage(self, damage_report: Report):
         total_damage = 0
