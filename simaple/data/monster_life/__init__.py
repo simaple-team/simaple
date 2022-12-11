70eb9feee0c1c29733e45dc0859d4fef02910cd2
oleneyl <meson3241@gmail.com>
2022-12-11 12:32:34 +0900

    feat: add monster life

---
+++ simaple/data/monster_life/__init__.py
@@ -0,0 +1,31 @@
+"""
+Data Monsterlife
+labeling rule
+level:
+  - normal: availabla to most user and most time
+  - strong: available to few people
+  - specific: available at specific period
+"""
+
+import os
+from pathlib import Path
+
+from simaple.core import Stat
+from simaple.spec.loader import SpecBasedLoader
+from simaple.spec.repository import DirectorySpecRepository
+from simaple.system import MonsterlifeMob, NamedStat
+
+
+def get_monsterlife_loader() -> SpecBasedLoader:
+    repository = DirectorySpecRepository(str(Path(__file__).parent))
+    return SpecBasedLoader(repository)
+
+
+def get_normal_monsterlife_stat() -> Stat:
+    loader = get_monsterlife_loader()
+
+    monster_life_mobs: list[NamedStat] = loader.load_all(
+        query={"kind": "NamedStat", "level": "normal"}
+    )
+
+    return sum((mob.get_stat() for mob in monster_life_mobs), Stat())
