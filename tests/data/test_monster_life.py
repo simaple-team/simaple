373c872844799742602f00e22cb0fe3c8aaf4d37
oleneyl <meson3241@gmail.com>
2022-12-11 15:41:03 +0900

    fix: minor typo

--- tests/data/test_monster_life.py
+++ tests/data/test_monster_life.py
@@ -2,7 +2,7 @@ from simaple.core import Stat
 from simaple.data.monster_life import get_normal_monsterlife_stat
 
 
-def test_normal_doping_data():
+def test_normal_monster_life_data():
     normal_monsterlife_stat = get_normal_monsterlife_stat()
     assert isinstance(normal_monsterlife_stat, Stat)
     assert normal_monsterlife_stat != Stat()
