0e3f212e678cd298600f1588df7bb3822d6db161
jj9810 <cheesej98@gmail.com>
2022-12-12 01:59:23 -0700

    feat: implement hyperstat parser

--- simaple/fetch/element/character/extractor.py
+++ simaple/fetch/element/character/extractor.py
@@ -3,6 +3,7 @@ from abc import ABCMeta, abstractmethod
 
 import bs4
 import pydantic
+import re
 
 
 class NumberString:
@@ -106,13 +107,48 @@ class CharacterAbilityExtractor(CharacterPropertyExtractor):
 
 class CharacterHyperstatExtractor(CharacterPropertyExtractor):
     def extract(self, soup):
-        return [
+        '''
+        # STR, DEX, INT, LUK, HP%, MP%, DF/TF/PP/영력, 크확, 크뎀, 방무, 데미지, 보뎀, 일뎀, 내성, 공마, 경험치, 아케인포스
+        # 스탯에는 스탯퍼 미적용
+        '''
+        stat_elements = [
             el.text
             for el in soup.select_one(
                 ".tab01_con_wrap table:nth-of-type(2) tr:nth-last-of-type(1) td span"
             ).children
             if isinstance(el, bs4.element.NavigableString)
         ]
+        regexes = {
+            "STR": re.compile('힘 \d+ 증가'),
+            "DEX": re.compile('민첩성 \d+ 증가'),
+            "INT": re.compile('지력 \d+ 증가'),
+            "LUK": re.compile('운 \d+ 증가'),
+            "HP": re.compile('최대 HP \d+% 증가'),
+            "MP": re.compile('최대 MP \d+% 증가'),
+            "DF": re.compile('최대 데몬 포스/타임 포스 \d+ 증가'),
+            "PP": re.compile('최대 싸이킥 포인트 \d+ 증가'),
+            "critical_rate": re.compile('크리티컬 확률 \d+% 증가'),
+            "critical_damage": re.compile('크리티컬 데미지 \d+% 증가'),
+            "ignored_defence": re.compile('방어율 무시 \d+% 증가'),
+            "damage_multiplier": re.compile('데미지 \d+% 증가'),
+            "boss_damage_multiplier": re.compile('보스 몬스터 공격 시 데미지 \d+% 증가'),
+            "immunity": re.compile('상태이상 내성 \d+ 증가'),
+            "attack_power": re.compile('공격력과 마력 \d+ 증가'),
+            "magic_attack": re.compile('공격력과 마력 \d+ 증가'),
+            "arcaneforce": re.compile('아케인포스 \d+ 증가'),
+        }
+        result =   {"STR": 0, "DEX": 0, "INT": 0, "LUK": 0,
+                    "HP": 0, "MP": 0, "DF": 0, "PP": 0,
+                    "critical_rate": 0, "critical_damage": 0, "ignored_defence": 0,
+                    "damage_multiplier": 0, "boss_damage_multiplier": 0,
+                    "immunity": 0, "attack_power": 0, "magic_attack": 0,
+                    "arcaneforce": 0
+                    }
+        for element in stat_elements:
+            for key in result.keys():
+                if regexes[key].match(element):
+                    result[key] = float(re.findall("\d+", element)[0])
+        return result
 
 
 class CharacterTraitExtractor(CharacterPropertyExtractor):
