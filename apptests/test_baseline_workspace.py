06a4b5807a74968aa16238b5677b1e0225b29214
oleneyl <meson3241@gmail.com>
2023-01-02 19:21:03 +0900

    Feature/baseline configuration (#126)
    * feat: add baseline configuration
    
    * test: extract app-level test
    
    * ci: seperate app test from unittest command
    
    * feat: add record.tsv as fixture

---
+++ apptests/test_baseline_workspace.py
@@ -0,0 +1,14 @@
+def test_read_main(baseline_configuration, client):
+    response = client.post(
+        "/workspaces/baseline",
+        json=baseline_configuration,
+    )
+    print(response.json())
+
+    assert response.status_code == 200
+    simulator_id = response.json()["id"]
+
+    resp = client.get(
+        f"/workspaces/logs/{simulator_id}/0",
+    )
+    assert resp.json()["index"] == 0
