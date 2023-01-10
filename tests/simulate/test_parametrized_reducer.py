d703e63494c9230a1a84cb7cb2616fa2ebdcb823
oleneyl <meson3241@gmail.com>
2023-01-10 19:24:22 +0900

    feat: parametrized reducer

---
+++ tests/simulate/test_parametrized_reducer.py
@@ -0,0 +1,66 @@
+from typing import Any
+
+from simaple.core.base import ActionStat
+from simaple.simulate.base import (
+    Action,
+    AddressedStore,
+    ConcreteStore,
+    Entity,
+    Environment,
+    Event,
+)
+from simaple.simulate.component.base import Component, ReducerState, reducer_method
+from simaple.simulate.global_property import GlobalProperty
+
+
+class SomeEntity(Entity):
+    obj: int = 3
+
+
+class SomeTestState(ReducerState):
+    some_state: SomeEntity
+
+
+class ViewTestComponent(Component):
+    value: int = 5
+
+    def get_default_state(self):
+        return {"some_state": SomeEntity()}
+
+    @reducer_method
+    def some_reducer(self, payload: None, state: SomeTestState):
+        return payload, [Event(name=self.name, payload=payload)]
+
+
+def test_paramterizd_reducer():
+    store = AddressedStore(ConcreteStore())
+    global_property = GlobalProperty(ActionStat())
+    global_property.install_global_properties(store)
+
+    environment = Environment(store=store)
+
+    component = ViewTestComponent(
+        name="test_component",
+        listening_actions={
+            "some_action.use": "some_reducer",
+            "some_parametrized_action.use": {"name": "some_reducer", "payload": {3: 5}},
+        },
+    )
+
+    component.add_to_environment(environment)
+
+    assert environment.resolve(
+        Action(
+            name="some_parametrized_action",
+            method="use",
+            payload={"value": 1324},
+        )
+    )[0].payload == {3: 5}
+
+    assert environment.resolve(
+        Action(
+            name="some_action",
+            method="use",
+            payload={"value": 1324},
+        )
+    )[0].payload == {"value": 1324}
