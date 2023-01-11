55c04475161f752dbe090639b82ae3ad5696e119
oleneyl <meson3241@gmail.com>
2023-01-11 21:50:48 +0900

    feat: add cygnus blessing

---
+++ simaple/simulate/component/specific/cygnus.py
@@ -0,0 +1,72 @@
+from typing import Optional
+
+from simaple.core.base import Stat
+from simaple.simulate.component.base import ReducerState, reducer_method, view_method
+from simaple.simulate.component.entity import Cooldown, Lasting
+from simaple.simulate.component.skill import SkillComponent
+from simaple.simulate.component.trait.impl import BuffTrait, CooldownValidityTrait
+from simaple.simulate.component.view import Running
+from simaple.simulate.global_property import Dynamics
+
+
+
+class TranscendentCygnusBlessingState(ReducerState):
+    cooldown: Cooldown
+    lasting: Lasting
+    dynamics: Dynamics
+
+
+class TranscendentCygnusBlessing(SkillComponent, BuffTrait, CooldownValidityTrait):
+    cooldown_duration: float
+    delay: float
+    lasting_duration: float
+
+    damage_increment: float
+    increase_interval: float
+    default_damage: float
+    maximum_damage: float
+
+    def get_default_state(self):
+        return {
+            "cooldown": Cooldown(time_left=0),
+            "lasting": Lasting(time_left=0),
+        }
+
+    @reducer_method
+    def use(self, _: None, state: TranscendentCygnusBlessingState):
+        return self.use_buff_trait(state)
+
+    @reducer_method
+    def elapse(self, time: float, state: TranscendentCygnusBlessingState):
+        return self.elapse_buff_trait(time, state)
+
+    @view_method
+    def validity(self, state: TranscendentCygnusBlessingState):
+        return self.validity_in_cooldown_trait(state)
+
+    @view_method
+    def buff(self, state: TranscendentCygnusBlessingState) -> Optional[Stat]:
+        if state.lasting.enabled():
+            return self.get_infinity_effect(state)
+
+        return None
+
+    @view_method
+    def running(self, state: TranscendentCygnusBlessingState) -> Running:
+        return Running(
+            name=self.name,
+            time_left=state.lasting.time_left,
+            lasting_duration=self._get_lasting_duration(state),
+        )
+
+    def get_infinity_effect(self, state: TranscendentCygnusBlessingState) -> Stat:
+        elapsed = state.lasting.get_elapsed_time()
+        tick_count = elapsed // self.increase_interval
+        damage_multiplier = min(
+            self.default_damage + self.damage_increment * tick_count,
+            self.maximum_damage,
+        )
+        return Stat(damage_multiplier=damage_multiplier)
+
+    def _get_lasting_duration(self, state) -> float:
+        return self.lasting_duration
---
+++ tests/simulate/component/specific/test_cygnus_blessing.py
@@ -0,0 +1,64 @@
+# pylint: disable=W0621
+import pytest
+
+from simaple.core.base import Stat
+from simaple.simulate.component.specific.cygnus import TranscendentCygnusBlessing, TranscendentCygnusBlessingState
+from simaple.simulate.global_property import Dynamics
+
+
+@pytest.fixture(name="cygnus_blessing_component")
+def fixture_cygnus_blessing_component():
+    return TranscendentCygnusBlessing(
+        name="초월자 시그너스의 축복",
+        cooldown_duration=180_000,
+        delay=600,
+        lasting_duration=110_000,
+        damage_increment=3,
+        increase_interval=3_000,
+        default_damage=70,
+        maximum_damage=115,
+    )
+
+
+@pytest.fixture(name="cygnus_blessing_state")
+def fixture_cygnus_blessing_state(cygnus_blessing_component: TranscendentCygnusBlessing, dynamics: Dynamics):
+    return TranscendentCygnusBlessingState.parse_obj(
+        {**cygnus_blessing_component.get_default_state(), "dynamics": dynamics}
+    )
+
+
+def test_cygnus_blessing_increment(
+    cygnus_blessing_component: TranscendentCygnusBlessing, cygnus_blessing_state: TranscendentCygnusBlessingState
+):
+    # when
+    state, _ = cygnus_blessing_component.use(None, cygnus_blessing_state)
+
+    # then
+    assert cygnus_blessing_component.buff(state) == Stat(damage_multiplier=70)
+
+
+def test_cygnus_blessing_increment_during_increase(
+    cygnus_blessing_component: TranscendentCygnusBlessing, cygnus_blessing_state: TranscendentCygnusBlessingState
+):
+    # when
+    state, _ = cygnus_blessing_component.use(None, cygnus_blessing_state)
+    state, _ = cygnus_blessing_component.elapse(18_000, state)
+
+    # then
+    assert cygnus_blessing_component.buff(state) == Stat(damage_multiplier=70 + 6 * 3)
+
+
+def test_maximum_cygnus_blessing_increment(
+    cygnus_blessing_component: TranscendentCygnusBlessing, cygnus_blessing_state: TranscendentCygnusBlessingState
+):
+    state, _ = cygnus_blessing_component.use(None, cygnus_blessing_state)
+    state, _ = cygnus_blessing_component.elapse(100_000, state)
+
+    assert cygnus_blessing_component.buff(state) == Stat(damage_multiplier=115)
+
+
+def test_cygnus_blessing_stop(cygnus_blessing_component: TranscendentCygnusBlessing, cygnus_blessing_state: TranscendentCygnusBlessingState):
+    state, _ = cygnus_blessing_component.use(None, cygnus_blessing_state)
+    state, _ = cygnus_blessing_component.elapse(140_000, state)
+
+    assert cygnus_blessing_component.buff(state) is None
