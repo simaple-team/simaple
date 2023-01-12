7b95f2feffb0f8299fc101fa1f78f1aa8aa40905
OniOniOn- <0onionion0@gmail.com>
2023-01-13 06:28:53 +0900

    fix: fix cygnus import

--- simaple/simulate/component/specific/__init__.py
+++ simaple/simulate/component/specific/__init__.py
@@ -3,6 +3,7 @@ import simaple.simulate.component.specific.archmagefb
 import simaple.simulate.component.specific.archmagetc
 import simaple.simulate.component.specific.bishop
 import simaple.simulate.component.specific.common_v
+import simaple.simulate.component.specific.cygnus
 import simaple.simulate.component.specific.dualblade
 import simaple.simulate.component.specific.flora
 import simaple.simulate.component.specific.magician
