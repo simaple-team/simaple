ef698c53c92df22e484130f5ad7291d767dae177
oleneyl <meson3241@gmail.com>
2023-07-27 21:58:18 +0900

    feat: show all specs

--- simaple/app/interface/routers/skill.py
+++ simaple/app/interface/routers/skill.py
@@ -1,7 +1,7 @@
 import fastapi
 from dependency_injector.wiring import inject
 
-from simaple.app.application.query.skill import get_skill
+from simaple.app.application.query.skill import get_skill, get_all_skill
 from simaple.app.domain.uow import UnitOfWork
 from simaple.app.interface.routers.base import UowProvider
 from simaple.spec.spec import Spec
@@ -9,9 +9,19 @@ from simaple.spec.spec import Spec
 skill_router = fastapi.APIRouter(prefix="/skill")
 
 
+@skill_router.get("/")
+@inject
+def get_all_skill_spec(
+    uow: UnitOfWork = UowProvider,
+) -> list[Spec]:
+    skill_specs = get_all_skill(uow)
+
+    return skill_specs
+
+
 @skill_router.get("/{skill_id}")
 @inject
-def get_all_component_spec(
+def get_skill_spec(
     skill_id: str,
     uow: UnitOfWork = UowProvider,
 ) -> Spec:
@@ -20,3 +30,4 @@ def get_all_component_spec(
         raise fastapi.HTTPException(status_code=404, detail="Skill not found")
 
     return skill_spec
+
