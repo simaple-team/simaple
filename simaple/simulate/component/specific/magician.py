d9eb2d8732428dfde86c2bded0d7642b9fde4f2b
icepeng <typ0@naver.com>
2023-07-27 21:14:29 +0900

    fix(simulate): buff view lasting duration

--- simaple/simulate/component/specific/magician.py
+++ simaple/simulate/component/specific/magician.py
@@ -55,7 +55,7 @@ class Infinity(SkillComponent, BuffTrait, CooldownValidityTrait):
         return Running(
             name=self.name,
             time_left=state.lasting.time_left,
-            lasting_duration=self._get_lasting_duration(state),
+            lasting_duration=state.lasting.assigned_duration,
         )
 
     def get_infinity_effect(self, state: InfinityState) -> Stat:
