d0f17186f589079762dd6d210346e4ba76d2274b
oleneyl <meson3241@gmail.com>
2024-01-01 21:28:53 +0900

    feat: add hexa improvement patch for v skills

--- simaple/data/skill/patch.py
+++ simaple/data/skill/patch.py
@@ -29,3 +29,31 @@ class VSkillImprovementPatch(Patch):
         output["modifier"] = new_modifier.short_dict()
 
         return output
+
+
+class HexaSkillImprovementPatch(Patch):
+    improvements: dict[str, int] = pydantic.Field(default_factory=dict)
+
+    def _compute_final_damage_multiplier(self, level: int) -> Stat:
+        if level == 0:
+            return Stat()
+        if 0 < level < 10:
+            return Stat(final_damage_multiplier=10 + level)
+        if 10 <= level < 20:
+            return Stat(final_damage_multiplier=15 + level)
+        if 20 <= level < 30:
+            return Stat(final_damage_multiplier=20 + level)
+        if level == 30:
+            return Stat(final_damage_multiplier=60)
+
+        raise ValueError(f"Invalid level: {level}")
+
+    def apply(self, raw: dict) -> dict:
+        output = copy.deepcopy(raw)
+        previous_modifier = Stat.model_validate(output.get("modifier", {}))
+
+        level = self.improvements.get(output["name"], 0)
+        new_modifier = previous_modifier + self._compute_final_damage_multiplier(level)
+        output["modifier"] = new_modifier.short_dict()
+
+        return output
