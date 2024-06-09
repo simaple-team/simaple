834cd7b1e6d7de1fc81675ceef045c215492020a
oleneyl <meson3241@gmail.com>
2024-06-09 19:20:33 +0900

    fix: number change due to artifact

--- tests/simulate/policy/test_dsl_runtime.py
+++ tests/simulate/policy/test_dsl_runtime.py
@@ -48,6 +48,6 @@ def test_dsl(dsl_list: list[str], dsl_test_setting: SimulationSetting) -> None:
     print(
         f"{engine.get_current_viewer()('clock')} | {container.dpm_calculator().calculate_dpm(report):,} "
     )
-    assert 10735001153803.367 == pytest.approx(
+    assert 10438982168263.46 == pytest.approx(
         container.dpm_calculator().calculate_dpm(report)
     )
