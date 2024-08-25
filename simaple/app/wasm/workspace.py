86befc89608da9879ed93498006b0bd6b9a4394f
oleneyl <meson3241@gmail.com>
2024-08-25 21:32:13 +0900

    feat: add simulator from minimal conf

--- simaple/app/wasm/workspace.py
+++ simaple/app/wasm/workspace.py
@@ -1,10 +1,18 @@
 from simaple.app.wasm.base import MaybePyodide, SessionlessUnitOfWork, pyodide_reveal_dict
-from simaple.simulate.interface.simulator_configuration import BaselineConfiguration
+from simaple.simulate.interface.simulator_configuration import BaselineConfiguration, MinimalSimulatorConfiguration
 
 from simaple.app.application.command.simulator import (
     create_simulator,
 )
 
+
+def createSimulatorFromMinimalConf(
+    conf: MaybePyodide,
+    uow: SessionlessUnitOfWork,
+):
+    baseline_conf = MinimalSimulatorConfiguration.model_validate(pyodide_reveal_dict(conf))
+    simulator_id = create_simulator(baseline_conf, uow)
+    return simulator_id
 
 
 def createSimulatorFromBaseline(
@@ -14,9 +22,4 @@ def createSimulatorFromBaseline(
     baseline_conf = BaselineConfiguration.model_validate(pyodide_reveal_dict(conf))
     simulator_id = create_simulator(baseline_conf, uow)
     return simulator_id
-
-
-
-def createSimulatorFromMinimalConf():
-    ...
 
