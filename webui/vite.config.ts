61bb31036436bd99d2ec60a5ef1a189754050ecf
icepeng <typ0@naver.com>
2024-09-11 08:35:06 +0900

    fix(webui): production bundle

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -1,10 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { copyFile, mkdir } from "node:fs/promises";
 import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  optimizeDeps: { exclude: ["pyodide"] },
+  plugins: [
+    react(),
+    {
+      name: "vite-plugin-pyodide",
+      generateBundle: async () => {
+        const assetsDir = "dist/assets";
+        await mkdir(assetsDir, { recursive: true });
+        const files = [
+          "pyodide-lock.json",
+          "pyodide.asm.js",
+          "pyodide.asm.wasm",
+          "python_stdlib.zip",
+        ];
+        for (const file of files) {
+          await copyFile(
+            path.join("node_modules/pyodide", file),
+            path.join(assetsDir, file),
+          );
+        }
+      },
+    },
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
