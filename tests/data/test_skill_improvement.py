3f73170ae23de33ec43b591c25650acaf7f5e909
oleneyl <meson3241@gmail.com>
2024-09-15 08:45:41 +0900

    feat: use skill improvement patch for other-skill relation (#228)
    * feat: use skill improvement patch for other-skill relation
    
    * style: lint
    
    * fix: missing name

---
+++ tests/data/test_skill_improvement.py
@@ -0,0 +1,29 @@
+from simaple.data.jobs.definitions.skill_improvement import SkillAdditiveImprovement
+
+
+def test_skill_improvement():
+    improvement = SkillAdditiveImprovement.model_validate(
+        {
+            "name": "VV",
+            "advantages": [
+                {
+                    "target_name": "x",
+                    "target_field": "f",
+                    "value": 10,
+                },
+                {
+                    "target_name": "y",
+                    "target_field": "f",
+                    "value": 10,
+                },
+            ],
+        }
+    )
+
+    original = {"name": "x", "f": 10, "g": 20}
+
+    assert {
+        "name": "x",
+        "f": 20,
+        "g": 20,
+    } == improvement.modify(original)
