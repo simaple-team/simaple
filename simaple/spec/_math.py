b35a264211cc084663970014e62839de6104853c
oleneyl <meson3241@gmail.com>
2024-09-14 17:05:07 +0900

    feat: add arithmetic parser

---
+++ simaple/spec/_math.py
@@ -0,0 +1,81 @@
+from lark import Lark, Transformer
+import math
+
+__grammar = r"""
+    ?start: expr
+
+    ?expr: expr "+" term   -> add
+         | expr "-" term   -> sub
+         | term
+
+    ?term: term "*" factor -> mul
+         | term "/" factor -> div
+         | term "//" factor -> int_div
+         | factor
+
+    ?factor: NUMBER        -> number
+           | SEPERATED_NUMBER -> seperated_number
+           | "ceil(" expr ")" -> ceil
+           | "floor(" expr ")" -> floor
+           | "min(" expr "," expr ")" -> min
+           | "max(" expr "," expr ")" -> max
+           | VARIABLE -> variable
+           | "-" factor    -> neg
+           | "(" expr ")"
+
+    SEPERATED_NUMBER: /[0-9_]+/
+    VARIABLE: /[a-zA-Z_\.]+/
+    %import common.NUMBER
+    %import common.WS_INLINE
+    %ignore WS_INLINE
+"""
+
+class CalcTransformer(Transformer):
+    def __init__(self, variables: dict[str, int|float]):
+        self.variables = variables
+
+    def variable(self, token):
+        assert token[0].value in self.variables, f"Variable {token[0].value} is not defined"
+        return self.variables[token[0].value]
+
+    def number(self, token):
+        return float(token[0])
+
+    def seperated_number(self, token):  
+        return float(token[0].replace('_', ''))
+
+    def add(self, items):
+        return items[0] + items[1]
+
+    def sub(self, items):
+        return items[0] - items[1]
+
+    def mul(self, items):
+        return items[0] * items[1]
+
+    def div(self, items):
+        return items[0] / items[1]
+
+    def int_div(self, items):
+        return items[0] // items[1]
+
+    def min(self, items):
+        return min(items[0], items[1])
+
+    def max(self, items):
+        return max(items[0], items[1])
+
+    def neg(self, items):
+        return -items[0]
+
+    def ceil(self, items):
+        return math.ceil(items[0])
+
+    def floor(self, items):
+        return math.floor(items[0])
+
+__arithmetic_parser = Lark(__grammar)
+
+def evaluate_expression(expression, variables: dict[str, int|float]) -> int|float:
+    ast =  __arithmetic_parser.parse(expression)
+    return CalcTransformer(variables).transform(ast)
---
+++ tests/spec/test_math.py
@@ -0,0 +1,43 @@
+from simaple.spec._math import evaluate_expression
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "arithmetic_expr, variables, expected", 
+    [
+        (
+            "3 + 4 * (2 - 1) / 5", {}, 3.8,
+        ),
+        (
+            "val + 4 * (2 - 1) / 5", {"val": 3}, 3.8,
+        ),
+        (
+            "(35 + skill_level // 2) * 0.01", {"skill_level": 31}, 0.5
+        ),
+        (
+            "min(35, 15) * 0.01", {}, 0.15
+        ),
+        (
+            "min(15, 45) * 0.01", {}, 0.15
+        ),
+        (
+            "(200 + skill_level * 3) * (1 - 0.01 * (10 + skill_level // 3)) + (300 + skill_level * 3) * 0.01 * (10 + skill_level // 3)",
+            {"skill_level": 30},
+            310
+        ),
+        (
+            "min(character_stat.INT // 2500, 15 + skill_level)",
+            {"character_stat.INT": 30000, "skill_level": 10},
+            12
+        ),
+        (
+            "ceil(3 + 4 * (2 - 1) / 5)",
+            {"character_stat.INT": 30000, "skill_level": 10},
+            4
+        )
+    ]
+)
+def test_math_expr(arithmetic_expr, variables, expected):
+    result = evaluate_expression(arithmetic_expr, variables)
+    assert pytest.approx(result) == expected
