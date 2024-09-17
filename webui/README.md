6e2aaffde7a9a00384d5cd5ea0c11c9ec0df8405
icepeng <typ0@naver.com>
2024-09-18 05:23:24 +0900

    perf: faster python package load

--- webui/README.md
+++ webui/README.md
@@ -15,8 +15,16 @@ yarn install
 
 ### Dev Server
 
+웹앱 개발 서버를 실행합니다.
+
 ```bash
 yarn dev
+```
+
+로컬에서 빌드된 simaple wheel을 Pyodide가 http로 접근할 수 있도록 서버를 추가로 실행해야 합니다.
+
+```bash
+yarn dev:wasm
 ```
 
 ### Build
--- webui/src/sdk/index.ts
+++ webui/src/sdk/index.ts
@@ -16,30 +16,22 @@ export async function loadPySimaple() {
     indexURL: "https://cdn.jsdelivr.net/pyodide/v0.26.2/full/",
   });
 
-  await pyodide.loadPackage("pydantic", { checkIntegrity: false });
-  await pyodide.loadPackage("micropip", { checkIntegrity: false });
-  await pyodide.loadPackage("sqlite3", { checkIntegrity: false });
-  await pyodide.loadPackage("lzma", { checkIntegrity: false });
+  await pyodide.loadPackage(["pydantic", "micropip", "sqlite3", "lzma"], {
+    checkIntegrity: false,
+  });
 
   const micropip = pyodide.pyimport("micropip");
-  await micropip.install("loguru");
-  await micropip.install("lark");
-  await micropip.install("numpy");
-  await micropip.install("pyyaml");
-  await micropip.install("pyfunctional");
+  await micropip.install(["loguru", "lark", "numpy", "pyyaml", "pyfunctional"]);
 
   if (import.meta.env.DEV) {
-    const handle = await window.showDirectoryPicker();
-    console.log(handle);
-
-    await pyodide.mountNativeFS("/tmp", handle);
+    await micropip.install(
+      "http://localhost:8000/simaple-0.4.2-py3-none-any.whl",
+      false,
+      false,
+    );
 
     return {
       pySimaple: pyodide.runPython(`
-    import sys
-    import os
-    sys.path.append('/tmp')
-    print(os.listdir('/tmp'))
     import simaple.wasm as wasm
     wasm`),
     };
