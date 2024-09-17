bbfd39e19ea649ff3897ae43f9f19e4b1657fa7a
oleneyl <meson3241@gmail.com>
2024-09-17 16:44:02 +0900

    feat: expose as root

--- simaple/wasm/__init__.py
+++ simaple/wasm/__init__.py
@@ -1,3 +1,12 @@
-from simaple.wasm.workspace import runSimulatorWithPlanConfig
+from simaple.wasm.skill import getAllSkillSpec, getSkillSpec
+from simaple.wasm.workspace import (
+    runSimulatorWithPlanConfig,
+    runSimulatorWithPlanConfigUsingCache,
+)
 
-__all__ = ["runSimulatorWithPlanConfig"]
+__all__ = [
+    "runSimulatorWithPlanConfig",
+    "runSimulatorWithPlanConfigUsingCache",
+    "getAllSkillSpec",
+    "getSkillSpec",
+]
