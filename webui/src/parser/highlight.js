38248d4d7bdeaac67d2b2a05330c2fd44701d663
icepeng <typ0@naver.com>
2024-09-18 20:45:39 +0900

    feat(webui): support more keyword and comment

--- webui/src/parser/highlight.js
+++ webui/src/parser/highlight.js
@@ -1,9 +1,12 @@
 import { styleTags, tags as t } from "@lezer/highlight";
 
 export const simapleHighlighting = styleTags({
-  Quotedstring: t.string,
+  QuotedString: t.string,
   Number: t.number,
   Repetition: t.operator,
   CastStatement: t.keyword,
   ElapseStatement: t.keyword,
+  ResolveStatement: t.keyword,
+  KeydownStopStatement: t.keyword,
+  Comment: t.lineComment,
 });
