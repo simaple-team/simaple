f17480de00a548d57f10c30a104cb5adac2c707a
icepeng <typ0@naver.com>
2024-09-18 10:09:42 +0900

    fix: missing damage calculation after simulation

--- simaple/wasm/models/simulation.py
+++ simaple/wasm/models/simulation.py
@@ -5,9 +5,8 @@ import pydantic
 from simaple.core.base import Stat
 from simaple.simulate.base import Action, Checkpoint, Event
 from simaple.simulate.component.view import Running, Validity
-from simaple.simulate.engine import OperationEngine
 from simaple.simulate.policy.base import Operation
-from simaple.simulate.report.base import DamageLog, SimulationEntry
+from simaple.simulate.report.base import SimulationEntry
 
 
 class _Report(pydantic.BaseModel):
@@ -38,52 +37,3 @@ class OperationLogResponse(pydantic.BaseModel):
     previous_hash: str
     operation: Operation
     index: int
-
-    @classmethod
-    def from_simulator(
-        cls, engine: OperationEngine, index: int
-    ) -> OperationLogResponse:
-        if index < 0:
-            index += len(engine.history())
-
-        operation_log = engine.history().get(index)
-
-        playlog_responses = []
-
-        for playlog in operation_log.playlogs:
-            viewer = engine.get_viewer(playlog)
-            entry = engine.get_simulation_entry(playlog)
-            damage_logs: list[DamageLog] = entry.damage_logs
-
-            damages = [
-                (damage_log.name, damage_log.damage) for damage_log in damage_logs
-            ]
-            damage = sum([damage_log.damage for damage_log in damage_logs])
-            playlog_responses.append(
-                PlayLogResponse(
-                    events=playlog.events,
-                    validity_view={v.name: v for v in viewer("validity")},
-                    running_view={v.name: v for v in viewer("running")},
-                    buff_view=viewer("buff"),
-                    clock=playlog.clock,
-                    report=_Report(time_series=[entry]),
-                    delay=playlog.get_delay_left(),
-                    action=playlog.action,
-                    checkpoint=playlog.checkpoint,
-                    damage=damage,
-                    damages=damages,
-                )
-            )
-
-        return OperationLogResponse(
-            index=index,
-            logs=playlog_responses,
-            hash=operation_log.hash,
-            previous_hash=operation_log.previous_hash,
-            operation=operation_log.operation,
-        )
-
-
-class FullOperationLogsWithCache(pydantic.BaseModel):
-    logs: list[OperationLogResponse]
-    cache: dict[str, str]
--- simaple/wasm/workspace.py
+++ simaple/wasm/workspace.py
@@ -5,8 +5,13 @@ from simaple.container.plan_metadata import PlanMetadata
 from simaple.container.simulation import SimulationContainer
 from simaple.simulate.policy.base import ConsoleText, Operation, is_console_command
 from simaple.simulate.policy.parser import parse_simaple_runtime
+from simaple.simulate.report.base import DamageLog
 from simaple.wasm.base import return_js_object_from_pydantic_list
-from simaple.wasm.models.simulation import OperationLogResponse
+from simaple.wasm.models.simulation import (
+    OperationLogResponse,
+    PlayLogResponse,
+    _Report,
+)
 
 
 @return_js_object_from_pydantic_list
@@ -20,9 +25,10 @@ def run(
     character_provider = CachedCharacterProvider.model_validate(
         serialized_character_provider
     )
-    engine = SimulationContainer(
+    simulation_container = SimulationContainer(
         metadata.simulation_setting, character_provider
-    ).operation_engine()
+    )
+    engine = simulation_container.operation_engine()
 
     for op_or_console in op_or_consoles:
         if is_console_command(op_or_console):
@@ -31,10 +37,51 @@ def run(
 
         engine.exec(cast(Operation, op_or_console))
 
-    return [
-        OperationLogResponse.from_simulator(engine, log_index)
-        for log_index in range(len(engine.history()))
-    ]
+    responses: list[OperationLogResponse] = []
+    history = engine.history()
+    damage_calculator = simulation_container.damage_calculator()
+
+    for idx, operation_log in enumerate(history):
+        playlog_responses = []
+
+        for playlog in operation_log.playlogs:
+            viewer = engine.get_viewer(playlog)
+            entry = engine.get_simulation_entry(playlog)
+            damage_logs: list[DamageLog] = entry.damage_logs
+
+            damages = [
+                (damage_log.name, damage_calculator.get_damage(damage_log))
+                for damage_log in damage_logs
+            ]
+            damage = damage_calculator.calculate_damage(entry)
+
+            playlog_responses.append(
+                PlayLogResponse(
+                    events=playlog.events,
+                    validity_view={v.name: v for v in viewer("validity")},
+                    running_view={v.name: v for v in viewer("running")},
+                    buff_view=viewer("buff"),
+                    clock=playlog.clock,
+                    report=_Report(time_series=[entry]),
+                    delay=playlog.get_delay_left(),
+                    action=playlog.action,
+                    checkpoint=playlog.checkpoint,
+                    damage=damage,
+                    damages=damages,
+                )
+            )
+
+        responses.append(
+            OperationLogResponse(
+                index=idx,
+                logs=playlog_responses,
+                hash=operation_log.hash,
+                previous_hash=operation_log.previous_hash,
+                operation=operation_log.operation,
+            )
+        )
+
+    return responses
 
 
 def getSerializedCharacterProvider(
