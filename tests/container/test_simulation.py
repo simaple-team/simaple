e94cc74d5b71b2166e22d06b118fb92153e2aa9a
oleneyl <meson3241@gmail.com>
2024-09-26 00:45:05 +0900

    feat: raise error if skill name not provided in skill profile

---
+++ tests/container/test_simulation.py
@@ -0,0 +1,127 @@
+import pytest
+
+from simaple.container.simulation import (
+    FinalCharacterStat,
+    SimulationContainer,
+    SimulationEnvironment,
+)
+from simaple.core import ActionStat, JobType, Stat
+from simaple.simulate.policy.base import Operation
+
+
+@pytest.mark.parametrize(
+    "wrong_envs",
+    [
+        SimulationEnvironment(
+            passive_skill_level=0,
+            combat_orders_level=0,
+            weapon_pure_attack_power=0,
+            jobtype=JobType.archmagefb,
+            level=280,
+            character=FinalCharacterStat(
+                stat=Stat(
+                    INT=1000,
+                    magic_attack=1000,
+                ),
+                action_stat=ActionStat(),
+            ),
+            hexa_skill_levels={
+                "undef 인페르날 베놈": 10,
+            },
+        ),
+        SimulationEnvironment(
+            passive_skill_level=0,
+            combat_orders_level=0,
+            weapon_pure_attack_power=0,
+            jobtype=JobType.archmagefb,
+            level=280,
+            character=FinalCharacterStat(
+                stat=Stat(
+                    INT=1000,
+                    magic_attack=1000,
+                ),
+                action_stat=ActionStat(),
+            ),
+            hexa_mastery_skill_levels={
+                "undef 인페르날 베놈": 10,
+            },
+        ),
+        SimulationEnvironment(
+            passive_skill_level=0,
+            combat_orders_level=0,
+            weapon_pure_attack_power=0,
+            jobtype=JobType.archmagefb,
+            level=280,
+            character=FinalCharacterStat(
+                stat=Stat(
+                    INT=1000,
+                    magic_attack=1000,
+                ),
+                action_stat=ActionStat(),
+            ),
+            hexa_improvements_levels={
+                "undef 인페르날 베놈": 10,
+            },
+        ),
+    ],
+)
+def test_simulation_with_improper_name_may_fail(wrong_envs):
+    with pytest.raises(AssertionError):
+        SimulationContainer(wrong_envs).builder()
+
+
+def test_simulation_environment_using_skill_levels():
+    # Test case 1: Verify the simulation environment with default skill levels
+    no_skill_environment = SimulationEnvironment(
+        passive_skill_level=0,
+        combat_orders_level=0,
+        weapon_pure_attack_power=0,
+        jobtype=JobType.archmagefb,
+        level=280,
+        character=FinalCharacterStat(
+            stat=Stat(
+                INT=1000,
+                magic_attack=1000,
+            ),
+            action_stat=ActionStat(),
+        ),
+        hexa_skill_level=1,
+        hexa_mastery_level=1,
+        v_improvements_level=60,
+        hexa_improvements_level=0,
+    )
+
+    environment_with_skill_level = SimulationEnvironment(
+        passive_skill_level=0,
+        combat_orders_level=0,
+        weapon_pure_attack_power=0,
+        jobtype=JobType.archmagefb,
+        level=280,
+        character=FinalCharacterStat(
+            stat=Stat(
+                INT=1000,
+                magic_attack=1000,
+            ),
+            action_stat=ActionStat(),
+        ),
+        hexa_skill_level=1,
+        hexa_mastery_level=1,
+        v_improvements_level=60,
+        hexa_improvements_level=0,
+        hexa_skill_levels={
+            "인페르날 베놈": 10,
+        },
+    )
+
+    engine = SimulationContainer(no_skill_environment).operation_engine()
+    engine_with_skill = SimulationContainer(
+        environment_with_skill_level
+    ).operation_engine()
+
+    op = Operation(
+        command="CAST",
+        name="인페르날 베놈",
+        expr="",
+    )
+
+    assert engine.exec(op) != engine_with_skill.exec(op)
