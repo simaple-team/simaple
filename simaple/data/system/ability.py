f2cfae12d390068a58f9cc9fd2c96317a7e39cf9
icepeng <typ0@naver.com>
2024-09-28 00:35:52 +0900

    refactor: move ability data to match system/

--- simaple/data/system/ability.py
+++ simaple/data/system/ability.py
@@ -6,7 +6,7 @@ from simaple.system.ability import AbilityLine
 
 
 def get_best_ability(jobtype: JobType) -> list[AbilityLine]:
-    with open(Path(__file__).parent / "best.json", encoding="utf-8") as f:
+    with open(Path(__file__).parent / "ability_fixed.json", encoding="utf-8") as f:
         best_abilities = json.load(f)
 
     ability_lines = best_abilities[jobtype.value]
