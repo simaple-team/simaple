688d6edc364739bcc64211acf3cb70b5b1ef50da
icepeng <typ0@naver.com>
2024-09-29 15:54:38 +0900

    feat(webui): change preferences page to popover

--- webui/src/pages/PreferencePopover.tsx
+++ webui/src/pages/PreferencePopover.tsx
@@ -1,18 +1,20 @@
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { usePreference } from "@/hooks/usePreference";
 import { Controller, useForm } from "react-hook-form";
 
-export function PreferencePage() {
+interface PreferencePopoverProps {
+  children?: React.ReactNode;
+}
+
+export function PreferencePopover(props: PreferencePopoverProps) {
   const { preferences, setPreferences } = usePreference();
   const { register, control, getValues, watch } = useForm({
     defaultValues: {
@@ -32,54 +34,55 @@ export function PreferencePage() {
   }
 
   return (
-    <div className="flex flex-col grow gap-2 p-4">
-      <Card>
-        <CardHeader>
-          <CardTitle>측정시간 설정</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="flex flex-col gap-3">
-            <div className="flex gap-2 items-center">
-              <Controller
-                name="useDuration"
-                control={control}
-                render={({ field: { value, onChange } }) => (
-                  <Checkbox
-                    id="useDuration"
-                    checked={value}
-                    onCheckedChange={onChange}
-                  />
-                )}
-              />
-              <Label htmlFor="useDuration">측정 시간 사용</Label>
-            </div>
-            <div className="flex gap-2 items-center">
+    <Popover>
+      <PopoverTrigger asChild>{props.children}</PopoverTrigger>
+      <PopoverContent className="w-96">
+        <div className="grid gap-4">
+          <div className="space-y-2">
+            <h4 className="font-medium leading-none">시간 설정</h4>
+            <p className="text-sm text-muted-foreground">
+              시뮬레이션 시작 시각과 측정 시간을 설정합니다.
+            </p>
+          </div>
+          <div className="flex items-center gap-2">
+            <Controller
+              name="useDuration"
+              control={control}
+              render={({ field: { value, onChange } }) => (
+                <Checkbox
+                  id="useDuration"
+                  checked={value}
+                  onCheckedChange={onChange}
+                />
+              )}
+            />
+            <Label htmlFor="useDuration">측정 시간 사용</Label>
+          </div>
+          <div className="grid gap-2">
+            <div className="grid grid-cols-2 items-center gap-4">
               <Label htmlFor="startClock">시작 시각 (초)</Label>
               <Input
                 id="startClock"
-                className="w-32"
+                className="h-8"
                 type="number"
                 {...register("startClock")}
               />
             </div>
-
             {watch("useDuration") && (
-              <div className="flex gap-2 items-center">
+              <div className="grid grid-cols-2 items-center gap-4">
                 <Label htmlFor="duration">측정 시간 (초)</Label>
                 <Input
                   id="duration"
-                  className="w-32"
+                  className="h-8"
                   type="number"
                   {...register("duration")}
                 />
               </div>
             )}
           </div>
-        </CardContent>
-        <CardFooter>
           <Button onClick={() => handleSave()}>저장</Button>
-        </CardFooter>
-      </Card>
-    </div>
+        </div>
+      </PopoverContent>
+    </Popover>
   );
 }
