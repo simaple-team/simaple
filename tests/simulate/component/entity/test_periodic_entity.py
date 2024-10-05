7b1ece732bc57c7aae181cc81c683d064e3b0df0
oleneyl <meson3241@gmail.com>
2024-10-06 01:14:02 +0900

    refactor: redesing periodic

--- tests/simulate/component/entity/test_periodic_entity.py
+++ tests/simulate/component/entity/test_periodic_entity.py
@@ -4,32 +4,50 @@ from simaple.simulate.component.entity import Periodic
 
 
 @pytest.mark.parametrize(
-    "time, expected",
+    "time, interval, expected_count",
     [
-        (1500, 12),
-        (300, 3),
-        (750, 7),
+        (1500, 100, 12),
+        (300, 100, 4),
+        (750, 100, 8),
     ],
 )
-def test_periodic_iterator(time, expected):
+def test_periodic_iterator(time, interval, expected_count):
     # given
-    periodic = Periodic(interval=100)
-    periodic.set_time_left(1200)
+    periodic = Periodic(interval=interval)
+    count = periodic.set_time_left(1200, initial_counter=0)
 
     # when
-    count = 0
     for _ in periodic.resolving(time):
         count += 1
 
     # then
-    assert count == expected
+    assert count == expected_count
+
+
+@pytest.mark.parametrize(
+    "time, interval, expected_count",
+    [
+        (1500, 100, 12),
+        (300, 100, 4),
+        (750, 100, 8),
+    ],
+)
+def test_elapse(time, interval, expected_count):
+    # given
+    periodic = Periodic(interval=interval)
+    count = periodic.set_time_left(1200, initial_counter=0)
+
+    # when
+    count += periodic.elapse(time)
+
+    # then
+    assert count == expected_count
 
 
 def test_periodic_iterator_partial():
     periodic = Periodic(interval=100)
-    periodic.set_time_left(1000)
+    count = periodic.set_time_left(1000, initial_counter=0)
 
-    count = 0
     times = []
     for time in range(50, 1050, 50):  # 50, ..., 1000
         for _ in periodic.resolving(50):
@@ -38,7 +56,7 @@ def test_periodic_iterator_partial():
 
     assert count == 10
     assert times[0] == 100
-    assert times[-1] == 1000
+    assert times[-1] == 900
 
 
 @pytest.mark.parametrize(
@@ -48,17 +66,18 @@ def test_periodic_iterator_partial():
         (0, 20, 70, 0),
         (150, 50, 70, 1),
         (200, 300, 500, 0),
-        (300, 300, 500, 1),
+        (300, 300, 500, 0),
+        (301, 300, 500, 1),
         (400, 300, 250, 0),
         (400, 300, 300, 1),
-        (400, 300, 400, 2),
+        (400, 400, 400, 0),
+        (400, 300, 400, 1),
     ],
 )
 def test_periodic_initial_counter(time_left, initial_counter, time, expected):
     periodic = Periodic(interval=100)
-    periodic.set_time_left(time_left, initial_counter)
+    count = periodic.set_time_left(time_left, initial_counter)
 
-    count = 0
     for _ in periodic.resolving(time):
         count += 1
 
