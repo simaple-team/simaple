e96b75901341d63447c460af5edcb1eeea40219c
icepeng <typ0@naver.com>
2024-10-09 22:01:38 +0900

    remove deleted components in init

--- simaple/simulate/component/common/__init__.py
+++ simaple/simulate/component/common/__init__.py
@@ -8,13 +8,8 @@ import simaple.simulate.component.common.dot_emitting_attack_skill
 import simaple.simulate.component.common.hit_limited_periodic_damage
 import simaple.simulate.component.common.keydown_skill
 import simaple.simulate.component.common.mob
-import simaple.simulate.component.common.multiple_attack_skill
-import simaple.simulate.component.common.multiple_hit_hexa_skill
-import simaple.simulate.component.common.periodic_damage_configurated_attack_skill
-import simaple.simulate.component.common.periodic_damage_configurated_hexa_skill
-import simaple.simulate.component.common.periodic_with_finish_skill
+import simaple.simulate.component.common.periodic_damage_skill
+import simaple.simulate.component.common.scheduled_damage_skill
 import simaple.simulate.component.common.stackable_buff_skill
-import simaple.simulate.component.common.synergy_skill
 import simaple.simulate.component.common.temporal_enhancing_attack_skill
 import simaple.simulate.component.common.triggable_buff_skill
-import simaple.simulate.component.common.triple_periodic_damage_hexa_skill
