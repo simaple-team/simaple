d28501d397ac1b277220d230a90eef9d04033c5d
oleneyl <meson3241@gmail.com>
2024-11-03 03:33:44 +0900

    refactor: hyperstat loader as single file

--- simaple/container/api_environment_provider.py
+++ simaple/container/api_environment_provider.py
@@ -5,7 +5,7 @@ from simaple.container.simulation import SimulationEnvironment
 from simaple.request.adapter.ability_loader import NexonAPIAbilityLoader
 from simaple.request.adapter.character_basic_loader import NexonAPICharacterBasicLoader
 from simaple.request.adapter.gear_loader.adapter import NexonAPIGearLoader
-from simaple.request.adapter.hyperstat_loader.adapter import NexonAPIHyperStatLoader
+from simaple.request.adapter.hyperstat_loader import NexonAPIHyperStatLoader
 from simaple.request.adapter.link_skill_loader.adapter import NexonAPILinkSkillLoader
 from simaple.request.adapter.propensity_loader.adapter import NexonAPIPropensityLoader
 from simaple.request.adapter.skill_loader.adapter import NexonAPICharacterSkillLoader
--- tests/request/adapter/test_hyperstat.py
+++ tests/request/adapter/test_hyperstat.py
@@ -1,6 +1,6 @@
 from simaple.core import StatProps
 from simaple.data.system.hyperstat import get_kms_hyperstat
-from simaple.request.adapter.hyperstat_loader.adapter import get_hyperstat_from_response
+from simaple.request.adapter.hyperstat_loader import get_hyperstat_from_response
 
 
 def test_hyperstat_adapter(character_hyper_stat_response):
